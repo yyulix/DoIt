5cb6274c0ae0929a2607c061ecaf0c2037fec40a
Yulia <yuliarandomchenko@gmail.com>
2021-10-27 00:53:30 +0300

    feature_register - final fixes 2

--- DoIt/Extensions/UIColorsExtension.swift
+++ DoIt/Extensions/UIColorsExtension.swift
@@ -10,15 +10,3 @@ import UIKit
 extension UIColor {
     static let accentColor = UIColor.systemTeal
 }
-
-extension UIViewController {
-    var topbarHeight: CGFloat {
-        var top = self.navigationController?.navigationBar.frame.height ?? 0.0
-        if #available(iOS 13.0, *) {
-            top += UIApplication.shared.windows.first?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
-        } else {
-            top += UIApplication.shared.statusBarFrame.height
-        }
-        return top
-    }
-}
