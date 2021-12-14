4cc64c16225606a31e27802da44764c43c2803c7
Yulia <yuliarandomchenko@gmail.com>
2021-12-15 01:14:04 +0300

    Network is done, but nothing works

--- DoIt/Models/Structures.swift
+++ DoIt/Models/Structures.swift
@@ -9,7 +9,7 @@ import UIKit
 import Firebase
 
 class Task {
-    let image: UIImage?
+    let image: URL?
     let title: String
     let description: String?
     let deadline: Date?
@@ -23,17 +23,45 @@ class Task {
         return Auth.auth().currentUser?.uid == uid
     }
     
-    init(image: UIImage?, title: String, description: String?, deadline: Date?, isDone: Bool, color: UIColor, uid: String = "") {
-        self.image = image
-        self.title = title
-        self.description = description
-        self.deadline = deadline
-        self.isDone = isDone
+    init(uid: User, id: String, dictionary: [String: AnyObject]) {
+        if let profileImageUrlString = dictionary["userPhotoUrl"] as? String {
+            guard let url = URL(string: profileImageUrlString) else {return}
+            self.image = url
+        }
+        self.title = dictionary["title"] as? String ?? ""
+        self.description = dictionary["description"] as? String ?? ""
+        if let deadlineTimestamp = dictionary["deadline"] as? Double {
+            self.deadline = Date(timeIntervalSince1970: deadlineTimestamp)
+        }
+        self.isDone = dictionary["is_done"] as? Bool ?? false
+        
+        // TODO: - FIX IT
         self.uid = ""
-        self.color = color
-        self.creationTime = Date()
-        self.chapterId = 0
+        
+        // TODO: - FIX IT
+        self.color = .systemBlue
+        
+        if let creationTimestamp = dictionary["timestamp"] as? Double {
+            self.creationTime = Date(timeIntervalSince1970: creationTimestamp)
+        }
+        
+        // NOT DONE
+        self.chapterId = dictionary["chapter_id"] as? Int ?? 0
     }
+    
+    // TODO: - FIX IT
+    
+//    init(image: UIImage?, title: String, description: String?, deadline: Date?, isDone: Bool, color: UIColor, uid: String = "") {
+//        self.image = image
+//        self.title = title
+//        self.description = description
+//        self.deadline = deadline
+//        self.isDone = isDone
+//        self.uid = ""
+//        self.color = color
+//        self.creationTime = Date()
+//        self.chapterId = 0
+//    }
 }
 
 struct Chapter {
